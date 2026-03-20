eea9531f144d56007d45724ca90cb6a53822e8ee
lucas.mrabinovich@gmail.com <lucas.mrabinovich@gmail.com>
2026-03-19 21:52:32 -0300

    Agrega comentario de prueba

--- main.js
+++ main.js
@@ -2,3 +2,6 @@
 function saludo(nombre, edad, mensaje, hora) {
   console.log(`${mensaje} ${nombre}. Tienes ${edad} años y son las ${hora}.`);
 }
+
+
+// prueba para ver la pantalla del pull request
